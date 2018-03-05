9bd8c3e918f0ccd7913e62ed51d9c062e98eda53
Harold Affo <affoharold@yahoo.fr>
2018-03-05 12:20:31 -0500

    adding frontend files to github pages

--- truffle.js
+++ truffle.js
@@ -12,6 +12,13 @@ module.exports = {
             port:8545,
             network_id:"4224"
             // ,from: '0xae62a4f3d2e8018ea882aaa0a4fd5d41b487722c'
+          },
+          rinkeby: {
+            host:"localhost",
+            port:8545,
+            network_id: 4, // rinkeby network id
+            gas: 4700000,
+            from: '0x54D2ced75Dfdc474D82dACe339953bB813A0F5c5'
           }
      }
 };
